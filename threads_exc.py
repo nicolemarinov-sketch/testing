8bf2792f85e43fa8be862d12c4922faa44a2ed42
nicolemarinov-sketch <nicolemarinov@gmail.com>
2026-08-23 10:31:15 +0300

    Rename and add threads_exc.py

--- threads_exc.py
+++ threads_exc.py
@@ -1,20 +1,22 @@
 from threading import Thread
- counter=0;
- counter_lock = lock # יצירת מנעול ושימוש במיוטקס
+counter=0
+counter_lock = Lock() # יצירת מנעול ושימוש במיוטקס
 def increment_counter():
     for i in range (100000):
-        global counter++
+        with counter_lock:
+            global counter += 1
 
 def decrement_counter():
     for i in range (100000):
-        global counter--
+        with counter_lock:
+            global counter -= 1
 
 thread1 = Thread(target = increment_counter)
-thread2 = Thread(target = ecrement_counter)
+thread2 = Thread(target = decrement_counter)
 thread1.start() # מאתחל את הטרד
 thread2.start()
 
 thread1.join()# מחכה במיין שכל הטרדים האחרים יסיימו לפני שממדיכים בקוד ומדפיסים את התוצאה
 thread2.join()
 
-print("the final value is: ",counter)
+print("the final counter  value is: ",counter)
