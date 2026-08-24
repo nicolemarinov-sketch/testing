0adb4fd6ed9caf33bd1fc7b81322e6882c1357de
nicolemarinov-sketch <nicolemarinov@gmail.com>
2026-08-24 12:39:08 +0300

    Add recursive file lister script

--- threads_exc.py
+++ threads_exc.py
@@ -4,12 +4,14 @@ counter_lock = Lock() # יצירת מנעול ושימוש במיוטקס
 def increment_counter():
     for i in range (100000):
         with counter_lock:
-            global counter += 1
+            global counter
+            counter += 1
 
 def decrement_counter():
     for i in range (100000):
         with counter_lock:
-            global counter -= 1
+            global counter
+            counter -= 1
 
 thread1 = Thread(target = increment_counter)
 thread2 = Thread(target = decrement_counter)
